fa7d0120750052a8cc4a3169b0704b1c24ee18cc
Rafsan <rafsan.quayes@gmail.com>
2020-12-21 01:16:56 +0600

    Did some stuff for when doctor's button is clicked

--- hospital/views.py
+++ hospital/views.py
@@ -15,7 +15,27 @@ def adminclick_view(request):
     return render(request,'hospital/adminclick.html')
 
 
+#for showing signup/login button for doctor(by sumit)
+def doctorclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/doctorclick.html')
 
+
+
+
+def admin_signup_view(request):
+    form=forms.AdminSigupForm()
+    if request.method=='POST':
+        form=forms.AdminSigupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('adminlogin')
+    return render(request,'hospital/adminsignup.html',{'form':form})
 
 
 
