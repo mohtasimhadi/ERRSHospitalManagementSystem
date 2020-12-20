1fe0897427c163b13be9a89042a83fb6f87b6694
Rafsan <rafsan.quayes@gmail.com>
2020-12-21 01:14:28 +0600

    Looking into how views work

--- hospital/views.py
+++ hospital/views.py
@@ -7,3 +7,22 @@ def home_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     return render(request, 'hospital/index.html')
+
+
+
+
+
+
+
+def admin_signup_view(request):
+    form=forms.AdminSigupForm()
+    if request.method=='POST':
+        form=forms.AdminSigupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('adminlogin')
+    return render(request,'hospital/adminsignup.html',{'form':form})
